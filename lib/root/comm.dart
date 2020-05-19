b74d3d967b398cbe6c8a7898d5d6a91ec15d42d4
Kevin Ren <kevinren.studio@gmail.com>
2020-05-19 09:01:21 +0800

    requestBuilder add dataType

--- lib/root/comm.dart
+++ lib/root/comm.dart
@@ -1,13 +1,17 @@
 import 'package:flutter/cupertino.dart';
 
+enum DataType { String, Map, Int, List }
+
 class RequestBuilder {
   final String url;
   final String token;
   final Map data;
+  final DataType dataType;
   final Map<String, dynamic> headerSetting;
 
   RequestBuilder({
     @required this.url,
+    this.dataType = DataType.Map,
     this.token,
     this.data,
     this.headerSetting,
