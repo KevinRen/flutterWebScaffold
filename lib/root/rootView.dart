ecc370986d4fbe1e0a853ac387592fe9b328ac8d
Kevin Ren <kevinren.studio@gmail.com>
2020-07-30 17:51:55 +0800

    RootView rebuild

--- lib/root/rootView.dart
+++ lib/root/rootView.dart
@@ -1,69 +1,69 @@
 import 'package:flutter/material.dart';
-import 'package:flutter/services.dart';
+import 'package:web_scaffold/web_scaffold.dart';
 import 'utils/http.dart';
 import 'utils/socket.dart';
 import 'comm/comm.dart';
 import 'comm.dart';
 import 'package:web_socket_channel/html.dart';
 
-typedef void OnKeyCallback(KeyInfo keyInfo);
-
-enum KeyType { keyDown, keyUp }
-
-class KeyInfo {
-  final String code;
-  final KeyType keyType;
-  KeyInfo({ @required this.code, @required this.keyType });
+enum BuildType {
+  Root,
+  Page,
+  Component
 }
 
-class RootView {
-  final BuildContext context;
-  final SocketUtil socketUtil = SocketUtil();
+ThemeData defaultTheme = ThemeData(
+  platform: TargetPlatform.iOS,
+  highlightColor: Colors.transparent,
+  splashColor: Colors.transparent,
+  primaryColor: Colors.white,
+  backgroundColor: Colors.white,
+);
 
-  RootView(this.context);
+class RootView {
+  final SocketUtil socketUtil = SocketUtil();
 
   Widget build({
     @required Widget body,
-    OnKeyCallback onKeyCallback,
-    bool isPage = true,
-    Color backgroundColor: Colors.white
+    BuildType type = BuildType.Component,
+    Color backgroundColor: Colors.white,
+    ThemeData themeData,
   }) {
-    return isPage ? Scaffold(
-      backgroundColor: backgroundColor,
-      body: body == null ? Container() : RawKeyboardListener(
-        focusNode: FocusNode(),
-        onKey: (RawKeyEvent event) {
-          _keyOnManager(event, onKeyCallback);
-        },
-        child: body,
-      ),
-    ) : body == null ? Container() : RawKeyboardListener(
-        focusNode: FocusNode(),
-        onKey: (RawKeyEvent event) {
-          _keyOnManager(event, onKeyCallback);
-        },
-      child: body
-    );
-  }
-
-  _keyOnManager(RawKeyEvent key, OnKeyCallback onKey) async {
-    String _keyType = key.runtimeType.toString();
-    if (_keyType == 'RawKeyDownEvent' || _keyType == 'RawKeyUpEvent') {
-      RawKeyEventDataWeb data = key.data;
-
-      if (onKey != null) onKey(KeyInfo(code: data.code, keyType: _keyType == 'RawKeyDownEvent' ? KeyType.keyDown : KeyType.keyUp));
+    switch(type) {
+      case BuildType.Root:
+        return MaterialApp(
+          debugShowCheckedModeBanner: false,
+          theme: themeData == null ? defaultTheme : themeData,
+          onGenerateRoute: AppRouter.setupRouters,
+          home: Scaffold(
+            backgroundColor: backgroundColor,
+            body: body,
+          )
+        );
+      case BuildType.Page:
+        return Scaffold(
+          backgroundColor: backgroundColor,
+          body: body,
+        );
+      default:
+        return body;
     }
   }
 
-  gotoPage(String path, {Map query, Map arguments}) {
+  gotoPage(BuildContext context, {
+      String path,
+      Map query,
+      Map arguments
+    }
+  ) {
     List _path = path.split('.');
     String _query = query != null ? '${Comm.mapToQuery(query)}' : '';
     Navigator.of(context).pushNamed('/${_path[1]}$_query', arguments: arguments);
   }
 
-  Map pageParams() => ModalRoute.of(context).settings.arguments;
+  Map pageParams(BuildContext context) => ModalRoute.of(context).settings.arguments;
 
-  Map<String, String> pageQuery() {
+  Map<String, String> pageQuery(BuildContext context) {
     String _path = ModalRoute.of(context).settings.name;
     List<String> _pathSplit = _path.contains('?') ? _path.split('?') : List();
     Map<String, String> _query = Map();
